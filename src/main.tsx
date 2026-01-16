24d5f7164a4ab7a468d09661ac9d9600e22993fa
Ruturaj2003 <ruturajhb2003@gmail.com>
2026-01-16 12:11:22 +0530

    chore: add core app infrastructure (API client, store, query, providers)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App.tsx'
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/index.ts";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./config/reactQuery.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
+  </StrictMode>
+);
