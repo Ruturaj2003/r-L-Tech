84885b0167cf0a9f77f1a2a510b869eda2fd1db3
Ruturaj2003 <ruturajhb2003@gmail.com>
2026-01-16 12:39:39 +0530

    chore: install shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
-  return <div className="text-2xl">App Shell</div>;
+  return (
+    <div className="text-2xl">
+      App Shell
+      <Button variant={"ghost"}>Hello </Button>
+    </div>
+  );
 }
